12850a1625bad9593ab8ab2acafc6e22e5e92b99
Yavzkesta <48090660+Yavzkesta@users.noreply.github.com>
2023-11-27 14:18:28 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://hebergtonserv.com',
+  url: 'https://delightful-sawine-e22144.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/knowledge',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
