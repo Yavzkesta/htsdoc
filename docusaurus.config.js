37ce06486c8837cdfecab37ecc2a441cdbb3e1ad
Yavzkesta <48090660+Yavzkesta@users.noreply.github.com>
2024-01-10 21:49:07 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://hebergtonserv.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -55,7 +55,7 @@ const config = {
       }),
     ],
   ],
-
+Docusaurus
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
